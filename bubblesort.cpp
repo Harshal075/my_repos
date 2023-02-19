e5f840c046426430cbf241c9153b7eb1b1508ee0
Harshal075 <patilharshal9041@gmail.com>
2023-02-19 19:03:56 +0530

    New selection added

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
 
-<<<<<<< HEAD
 void quicksort(int *arr , int n)
 {
-    //code for quicksort ;
-=======
-void selection_sort(int *arr , int n)
+    //code for quicksort ;git
+}
+
+void selection(int *arr)
 {
-    //code for selection sort;
->>>>>>> selection_sort
+    //
 }
 
 int main()
